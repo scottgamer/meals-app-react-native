dd2771c50107b469608386faeddc2d8f21c26301
Richard Munoz <richosojason@msn.com>
2019-10-19 00:42:04 -0500

    refactored categoryMealsScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,58 +1,25 @@
-import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import React from 'react';
 
-import { CATEGORIES, MEALS } from "../data/dummy-data";
-import MealItem from '../components/MealItem';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealList from '../components/MealList';
 
 const CategoryMealScreen = props => {
+  const catId = props.navigation.getParam('categoryId');
 
-  const renderMealItem = (itemData) => {
-    return <MealItem
-      title={itemData.item.title}
-      image={itemData.item.imageUrl}
-      duration={itemData.item.duration}
-      complexity={itemData.item.complexity}
-      affordability={itemData.item.affordability}
-      onSelectMeal={() => {
-        props.navigation.navigate('MealDetail', {
-          mealId: itemData.item.id
-        });
-       }} />
-  };
-
-  const catId = props.navigation.getParam("categoryId");
-
-  const displayedMeals = MEALS.filter((meal) => {
+  const displayedMeals = MEALS.filter(meal => {
     return meal.categoryIds.indexOf(catId) >= 0;
   });
 
-  return (
-    <View style={styles.screen}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item, index) => item.id}
-        renderItem={renderMealItem}
-        style={{ width: '100%' }}
-      />
-    </View>
-  );
+  return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
 CategoryMealScreen.navigationOptions = navigationData => {
-  const catId = navigationData.navigation.getParam("categoryId");
+  const catId = navigationData.navigation.getParam('categoryId');
   const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
   return {
     headerTitle: selectedCategory.title
   };
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
 
 export default CategoryMealScreen;
